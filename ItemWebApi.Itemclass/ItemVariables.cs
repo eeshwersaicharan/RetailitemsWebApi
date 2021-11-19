dcac475d67dbfc2cb49ec8a317d59b36e20e7777
seeshwar <seeshwar@IM-RT-LP-678>
2021-11-19 22:38:34 +0530

    Add project files.

---
+++ ItemWebApi.Itemclass/ItemVariables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ItemWebApi.Itemclass
+{
+    public class Items
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ ItemWebApi.Operations/IitemServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using ItemWebApi.Itemclass;
+
+namespace ItemWebApi.Operations
+{
+    public interface IitemServices
+    {
+        IEnumerable<Items> GetAllItems();
+        Items GetItemById();
+        IEnumerable<Items> GetItemByCategory();
+
+        bool InsertItem(Items _Item);
+        bool DeleteItem(int _Id);
+        bool UpdateItemPrice(int _Id);
+    }
+}
